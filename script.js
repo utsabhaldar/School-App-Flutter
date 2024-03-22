9d49ca40d442ec3b09e09efca58c78e82173d397
Utsab Haldar <utsabhaldar88888@gmail.com>
2024-03-23 00:59:22 +0530

    Home page header and body design & animation

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings :["Flutter Developer", "DSA Enthusiast", "App developer", "Creator"],
+    typeSpeed:30,
+    backSpeed:30,
+    backDelay:1100,
+    loop:true,
+})
